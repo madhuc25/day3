0d41061b6994964704dd6c03f539cec90fd9947a
madhuc97 <mc932099@gmail.com>
2021-08-25 09:59:42 +0530

    New commit

--- src/com/training/org/MainClass.java
+++ src/com/training/org/MainClass.java
@@ -1,4 +1,14 @@
 package com.training.org;
 
 public class MainClass {
+
+    public static void main(String[] args) {
+        Bank b1=new Bank();
+        System.out.println(b1);
+
+        Bank b2=new Bank("xyz", "shimoga", 1233);
+        System.out.println(b2);
+    }
+
+
 }
